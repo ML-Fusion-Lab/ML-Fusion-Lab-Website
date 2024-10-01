6902432dbf0ba9a9a7f6eac7f08204e7eec98803
Ashish Tiwari <iamashishtiwari203@gmail.com>
2024-10-02 02:10:14 +0530

    fixed the position of the navbar elements, added a hamburger icon for mobile devices, included animations for the main text on the landing page and the navbar elements, and made the features section responsive.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Welcome to ML Fusion Lab!");
 });
+
+let navSecond = document.getElementById('second-nav');
+let flag = 0;
+
+function activeNav(){
+
+    if (flag == 0) {
+        navSecond.style.display = 'block'
+        flag += 1;
+    }
+    else{
+        navSecond.style.display = 'none';
+        flag -= 1;
+    }
+} 
+
