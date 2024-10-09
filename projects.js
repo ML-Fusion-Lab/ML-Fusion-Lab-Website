f10cd35c797e24d0524da56a0fb9e9b336e77c62
ArchanRD <archanrd29@gmail.com>
2024-10-09 12:51:06 +0530

    Add load more button

---
+++ projects.js
@@ -0,0 +1,241 @@
+// Array of projects
+const projects = [
+    {
+      "modelid": "modal1",
+      "cardImage": "Assets/projectpics/crops.jpg",
+      "cardAlt": "crops",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Crop Height Using Computer Vision",
+      "cardDesc": "Utilize contour detection techniques to estimate the height of crops for agricultural purposes.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal2",
+      "cardImage": "Assets/projectpics/disease.jpg",
+      "cardAlt": "disease",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "AI-Based Disease Diagnosis",
+      "cardDesc": "A machine learning model that helps detect early symptoms of diseases using patient data.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal3",
+      "cardImage": "Assets/projectpics/shop.jpg",
+      "cardAlt": "shopping",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Recommendation System for Shopping",
+      "cardDesc": "Build a recommendation system that suggests products based on user browsing behavior.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal4",
+      "cardImage": "Assets/projectpics/cnn.png",
+      "cardAlt": "cnn",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Image Classification with Deep Learning",
+      "cardDesc": "Develop a model that classifies images into categories using convolutional neural networks.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal5",
+      "cardImage": "Assets/projectpics/emai.jpg",
+      "cardAlt": "email",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Automated Email Response System",
+      "cardDesc": "Implement a system that uses NLP to generate automated responses to common email inquiries.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal6",
+      "cardImage": "Assets/projectpics/stock.jpg",
+      "cardAlt": "stock",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Stock Price Time Series Forecasting",
+      "cardDesc": "Apply machine learning algorithms to predict future stock prices based on historical data.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal7",
+      "cardImage": "Assets/projectpics/bot.png",
+      "cardAlt": "bot",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Chatbot Development for Customer Support",
+      "cardDesc": "Create an AI-powered chatbot that assists users with their inquiries in real time.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal8",
+      "cardImage": "Assets/projectpics/fraud.png",
+      "cardAlt": "fraud",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "AI-Powered Fraud Detection System",
+      "cardDesc": "Develop a system that identifies fraudulent activities using machine learning techniques.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal9",
+      "cardImage": "Assets/projectpics/lth.png",
+      "cardAlt": "health",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Healthcare Predictive Analytics",
+      "cardDesc": "Use patient data to predict potential diseases, enabling early intervention and personalized treatment plans.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal10",
+      "cardImage": "Assets/projectpics/wild.jpg",
+      "cardAlt": "wild",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Wildfire Detection Using Satellite Images",
+      "cardDesc": "Leverage satellite imagery and deep learning to detect and predict wildfire occurrences in vulnerable regions.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal11",
+      "cardImage": "Assets/projectpics/waste.jpg",
+      "cardAlt": "waste",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Waste Management Optimization",
+      "cardDesc": "Develop a machine learning model to optimize waste collection schedules based on city data, reducing pollution.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal12",
+      "cardImage": "Assets/projectpics/traffic.jpg",
+      "cardAlt": "traffic",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Traffic Accident Prediction System",
+      "cardDesc": "Use real-time traffic data and predictive models to forecast traffic accidents and improve road safety.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal13",
+      "cardImage": "Assets/projectpics/personalizd.jpg",
+      "cardAlt": "personalized",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Personalized Learning Recommendation System",
+      "cardDesc": "Create an adaptive learning platform that suggests personalized learning paths based on student performance data.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal14",
+      "cardImage": "Assets/projectpics/mental.jpg",
+      "cardAlt": "mental",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "AI for Mental Health Analysis",
+      "cardDesc": "Utilize sentiment analysis and pattern recognition to assess mental health based on user interactions and data.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal15",
+      "cardImage": "Assets/projectpics/dist.jpg",
+      "cardAlt": "Disaster",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "AI-Driven Disaster Relief Supply Chain Optimization",
+      "cardDesc": "Optimize supply chain logistics for disaster relief efforts, predicting demand and supply shortages in real time.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal16",
+      "cardImage": "Assets/projectpics/sound.jpg",
+      "cardAlt": "sound",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Urban Sound Classification",
+      "cardDesc": "Build a system that classifies urban sounds (e.g., traffic, sirens, and birds) to aid in city planning and noise pollution analysis.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal17",
+      "cardImage": "Assets/projectpics/eneryg.png",
+      "cardAlt": "energy",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Energy Consumption Prediction for Smart Homes",
+      "cardDesc": "Create a model that predicts energy consumption in smart homes based on weather and user behavior data, optimizing energy usage.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal18",
+      "cardImage": "Assets/projectpics/nut.jpg",
+      "cardAlt": "nutrition",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "AI for Personalized Nutrition Plans",
+      "cardDesc": "Use user data (age, health conditions, dietary preferences) to develop personalized nutrition and meal plans using machine learning.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal19",
+      "cardImage": "Assets/projectpics/rec.jpg",
+      "cardAlt": "recycle",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "Plastic Waste Classification for Recycling",
+      "cardDesc": "Develop a system using computer vision to classify plastic waste into categories, helping streamline recycling processes.",
+      "learnMoreLink": "#"
+    },
+    {
+      "modelid": "modal20",
+      "cardImage": "Assets/projectpics/the.jpg",
+      "cardAlt": "Therapist",
+      "imgStyle": "box-shadow: none; border-radius: 5px;",
+      "cardTitle": "AI-Powered Virtual Therapist",
+      "cardDesc": "Create a conversational AI that provides mental health support and guidance based on user interactions, reducing barriers to therapy.",
+      "learnMoreLink": "#"
+    }
+  ]
+  
+  
+  // Select card container
+  const projectList = document.querySelector(".project-list");
+  const loadMoreButton = document.querySelector(".load-more-button");
+  // Render projects
+  cardPerPage = 5;
+  currentIndex = 0;
+  
+  const loadMore = () => {
+    for (
+      let i = currentIndex;
+      i < currentIndex + cardPerPage && i < projects.length;
+      i++
+    ) {
+      // Create card element
+      const card = document.createElement("div");
+      const cardImg = document.createElement("img");
+      const title = document.createElement("h3");
+      const desc = document.createElement("p");
+      const link = document.createElement("a");
+
+      // Set attributes
+      card.className = "project-item";
+  
+      cardImg.setAttribute("src", projects[i].cardImage);
+      cardImg.setAttribute("style", projects[i].imgStyle)
+      cardImg.setAttribute("alt", projects[i].cardAlt);
+        
+      link.href = projects[i].learnMoreLink;
+      link.innerHTML = "Learn more";
+      link.classList.add("btn");
+      link.setAttribute("onclick", `openModal('${projects[i].modelid}')`)
+      title.innerHTML = projects[i].cardTitle;
+      desc.innerHTML = projects[i].cardDesc;
+
+      // Append the elements
+      card.appendChild(cardImg);
+      card.appendChild(title);
+      card.appendChild(desc);
+      card.appendChild(link);
+  
+      projectList.appendChild(card);
+    }
+    currentIndex += cardPerPage;
+  
+    if (currentIndex > projects.length){
+      loadMoreButton.classList.add("hide-btn")
+    }
+  };
+  
+  // Load cards on click event
+  loadMoreButton.addEventListener("click", () => {
+    loadMore();
+  });
+  
+  // Function is called once statically to load initial cards.
+  loadMore();
